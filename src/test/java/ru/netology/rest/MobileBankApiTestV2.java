7f23bb77956bda4d38614d72030ef281e2359fd8
IrinaChapurina <97689565+IrinaChapurina@users.noreply.github.com>
2023-07-10 21:04:58 +0300

    add fail

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class MobileBankApiTestV2 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("every{ it.balance >= 0}", is(true));
+    }
+}
